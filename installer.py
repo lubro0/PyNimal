0f806116ca69c85a620fa9eb60b9fd5b31849ad8
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-18 23:27:55 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -5,7 +5,7 @@ os.system("pkg update && pkg upgrade")
 os.system("pkg install wget")
 os.system("clear")
 print("\033[93mInstalling Version...\033[0m")
-os.system("wget https://github.com/lubro0/PyNimal/blob/main/version.txt -O version.txt")
+os.system("wget https://github.com/lubro0/PyNimal/releases/download/newest/version.txt -O version.txt")
 os.system("clear")
 with open("version.txt", "r") as f:
     version = f.read().strip()
