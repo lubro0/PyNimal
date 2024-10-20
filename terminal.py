fd5fcb4c3b56cfc32824e6c9fcc261dd92af2505
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 18:20:34 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -18,7 +18,7 @@ while True:
         print("\033[91mSOON\033[0m")
     
     if choice.lower() == "exit":
-        os._exit(0)
+        break
 
     if choice.lower() == "reset":
         os.system("rm -rf config && rm -rf terminal.py && git clone --depth 1 --single-branch https://github.com/lubro0/PyNimal.git temp_dir && mv temp_dir/* . && rm -rf temp_dir && python installer.py")
