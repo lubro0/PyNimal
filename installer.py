2eefbc3592fad5f39aec298fe254aa1f6617dbba
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 18:03:26 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -5,5 +5,6 @@ os.system('clear')
 if os.path.exists("README.md"):
     os.remove("README.md")
 os.system('pip install asyncio requests')
+os.system('pkg install nano')
 os.remove("installer.py")
 os.system("python terminal.py")
