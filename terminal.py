0f96571f9ca37fc4573e22e78360027044c1f2db
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 16:41:36 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,3 +1,13 @@
 import os
+import time
 
 os.system('clear')
+print("Create folders...")
+time.sleep(1)
+
+os.makedirs("config", exist_ok=True)
+with open("config/discord_config.txt", "w") as file:
+    pass
+
+os.system('clear')
+print("\033[92mSuccessful\033[0m")
