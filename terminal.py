c3945b04c110c6bde556abf9a162c324b46d275f
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 18:04:56 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 os.system('clear')
 
@@ -6,3 +7,13 @@ with open("config/version.txt", "r") as file:
     version = file.read().strip()
 
 print(f"\033[90mPyNimal v{version}\033[0m")
+print("\n1> Create File")
+
+while True:
+    choice = input(">>> ")
+
+    if choice == "1":
+        print("\033[91mSoon\033[0m")
+        time.sleep(1)
+    else:
+        print("\033[91mInvalid\033[0m")
