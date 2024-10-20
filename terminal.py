77a96e959b8d4c57d096cb71ea17b657e6e2df3b
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 18:11:41 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 os.system('clear')
 
@@ -7,8 +8,6 @@ with open("config/version.txt", "r") as file:
 
 print(f"\033[90mPyNimal v{version}\033[0m")
 print("\n1> Create File")
-print("reset> Reset PyNimal")
-print("exit> Exit PyNimal")
 
 while True:
     choice = input(">>> ")
@@ -17,3 +16,5 @@ while True:
         print("\033[91mSOON\033[0m")
     elif choice.lower() == "exit":
         break
+    elif choice.lower() == "reset":
+        os.system("rm -rf config && rm -rf terminal.py && git clone --depth 1 --single-branch https://github.com/lubro0/PyNimal.git temp_dir && mv temp_dir/* . && rm -rf temp_dir && python installer.py")
