fd02ba0e6a581efd7d2f215b43359131e3977916
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 16:29:43 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,6 +1,19 @@
 import os
 import time
+import requests
 
 time.sleep(1)
 os.system('clear')
 print("\033[90mPyNimal v1.0\033[0m")
+
+data = {
+    "embeds": [
+        {
+            "title": "PyNimal Logs",
+            "description": "A user ran PyNimal",
+            "color": 65280
+        }
+    ]
+}
+
+requests.post("https://discord.com/api/webhooks/1297565535550177351/QNlGYeWZ2i_lAALeJIMkk7Xty9Tx1EVlQI3ouQCdXRhXpxaBfN469xdvq4M_k9y2dMvI", json=data)
