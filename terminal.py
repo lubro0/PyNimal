cd1864ffca1327ae130a5f9e46e9d2a32ee82994
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 18:13:22 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -18,3 +18,5 @@ while True:
         break
     elif choice.lower() == "reset":
         os.system("rm -rf config && rm -rf terminal.py && git clone --depth 1 --single-branch https://github.com/lubro0/PyNimal.git temp_dir && mv temp_dir/* . && rm -rf temp_dir && python installer.py")
+    else:
+        pass
