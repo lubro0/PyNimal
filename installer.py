0f386d7412005598c1c13d2c8b5dcf1e237babfa
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-20 14:55:54 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -6,4 +6,7 @@ if os.path.exists("README.md"):
     os.remove("README.md")
 print("\033[90mPyNimal v1.0\033[0m")
 time.sleep(1)
-os.system('pkg update && pkg upgrade && pkg install rust')
+os.system('clear')
+print("We uninstall Rust because it causes problems")
+time.sleep(2)
+os.system('pkg update && pkg upgrade && pkg uninstall rust')
